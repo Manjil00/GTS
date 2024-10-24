48aa1c1b2a96d774f01cf3a3e8ecb4a032176a3d
ACER <210220@softwarica.edu.np>
2024-10-24 23:29:06 +0545

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="main-container">
+
+  <div className="main">
+    <h1 className="text-center bg-red-800">SETUPPP!!!!!!!!!!!</h1>
+  </div>
+
+</div>
+);
 }
 
 export default App;
