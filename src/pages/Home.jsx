81204c654fb1546665843582c9fd61e05111cce0
ACER <210220@softwarica.edu.np>
2024-10-26 12:41:28 +0545

    sec2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ return (
             </div>
         </div>
 
-        <div className="countups flex justify-center items-center gap-5 absolute bottom-0 inset-x-6">
+    <div className="countups flex justify-center items-center gap-5 absolute bottom-0 inset-x-6">
         <div className='cards h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-green-800 rounded-xl p-3'>
                 <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold'>
                     <CountUp end={25} duration={6}></CountUp>+</h1>
@@ -38,8 +38,12 @@ return (
 
                 <p className='text-center mt-3 break-words text-xs md:text-xl'>qwertiueyrcscsdcdsccrda</p>
         </div>
-        </div>
-        
+    </div>
+    </div>
+
+    {/* //SECTION2 */}
+    <div className="Section2 h-[500px] w-full">
+
     </div>
 </div>
 )
