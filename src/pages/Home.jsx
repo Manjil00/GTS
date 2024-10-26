8151428d7e9200daee9620d482df9fd17b983d92
ACER <210220@softwarica.edu.np>
2024-10-26 12:36:31 +0545

    countup Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import CountUp from 'react-countup'
 //Components
 import Nav from '../components/Nav'
 
@@ -20,23 +20,26 @@ return (
                 <img src='../images/server1.jpg' className=' h-[150px] w-[150px] md:h-[400px] md:w-[400px]' alt='test'/>
             </div>
         </div>
+
         <div className="countups flex justify-center items-center gap-5 absolute bottom-0 inset-x-6">
-        <div className='cards h-[130px] w-[190px] bg-green-800 rounded-xl p-3'>
-                <h1 className='text-center mt-2 text-sm md:text-2xl'>100+</h1>
-                <p className='text-center mt-3 break-words text-xs md:text-xl'>qwertiueyruwecxcsdcscsdcdsccrda</p>
+        <div className='cards h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-green-800 rounded-xl p-3'>
+                <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold'>
+                    <CountUp end={25} duration={6}></CountUp>+</h1>
+                <p className='text-center mt-3 break-words text-xs md:text-xl'>qwertiueyruwecxcsda</p>
         </div>
+        <div className='cards2 h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-green-800 rounded-xl p-3'>
+                <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold'>
+                <CountUp end={25} duration={6}></CountUp>+</h1>
+                <p className='text-center mt-3 break-words text-xs md:text-xl'>qwertiueyruwecx</p>
+        </div>
+        <div className='cards3 h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-green-800 rounded-xl p-3'>
+                <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold'>
+                <CountUp end={25} duration={6}></CountUp>+</h1>
 
-        <div className='cards2 h-[130px] w-[190px] bg-green-800 rounded-xl p-3'>
-                <h1 className='text-center mt-2 text-sm md:text-2xl'>100+</h1>
-                <p className='text-center mt-3 break-words text-xs md:text-xl'>qwertiueyruwecxcsdcscsdcdsccrda</p>
+                <p className='text-center mt-3 break-words text-xs md:text-xl'>qwertiueyrcscsdcdsccrda</p>
         </div>
-
-        <div className='cards3 h-[130px] w-[190px] bg-green-800 rounded-xl p-3'>
-                <h1 className='text-center mt-2 text-sm md:text-2xl'>100+</h1>
-                <p className='text-center mt-3 break-words text-xs md:text-xl'>qwertiueyruwecxcsdcscsdcdsccrda</p>
         </div>
-
-        </div>
+        
     </div>
 </div>
 )
