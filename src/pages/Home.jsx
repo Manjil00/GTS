1c67bfa278613d455818ce9a9a4a3b1146d6f54f
ACER <210220@softwarica.edu.np>
2024-10-31 17:29:21 +0545

    sec4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,17 +18,17 @@ return (
         </div>
 
     <div className="countups flex justify-center items-center gap-5 absolute bottom-0 inset-x-6">
-        <div className='cards h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-blue-600  rounded-xl p-3'>
-                <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold text-white'>
+        <div className='cards h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-blue-600 cursor-pointer hover:bg-black  rounded-xl p-3'>
+                <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold text-white '>
                     <CountUp end={25} duration={6}></CountUp>+</h1>
                 <p className='text-center mt-3 break-words text-xs md:text-xl font-semibold text-white'>Happy Clients</p>
         </div>
-        <div className='cards2 h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-blue-600 rounded-xl p-3'>
+        <div className='cards2 h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-blue-600 cursor-pointer hover:bg-black  rounded-xl p-3'>
                 <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold text-white'>
                 <CountUp end={100} duration={6}></CountUp>+</h1>
                 <p className='text-center mt-3 break-words text-xs md:text-xl font-semibold text-white'>Maintained Servers</p>
         </div>
-        <div className='cards3 h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-blue-600 rounded-xl p-3'>
+        <div className='cards3 h-[100px] w-[100px] md:h-[130px] md:w-[190px] bg-blue-600 cursor-pointer hover:bg-black rounded-xl p-3'>
                 <h1 className='text-center md:mt-2 text-xl md:text-4xl font-bold text-white'>
                 <CountUp end={25} duration={6}></CountUp>+</h1>
 
@@ -98,6 +98,9 @@ We offer managed IT services for small to mid-sized organizations, businesses, a
     </div>
 
     {/* SECTION 4 */}
+    <div className="Section4 h-[500px] bg-red-800 w-full p-2">
+    
+    </div>
 </div>
 )
 }
