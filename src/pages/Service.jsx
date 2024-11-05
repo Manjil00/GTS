af59eaec55228a41fd12bf4e461104ee38954420
ACER <210220@softwarica.edu.np>
2024-11-05 20:57:44 +0545

    video service

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -12,19 +12,32 @@ const Service = () => {
       <h1 className='font-sans text-center text-2xl md:text-4xl'>Our Services</h1>
 
       <div className="card grid grid-cols-1 md:grid-cols-3 justify-center items-center ">
+
         <div className="CARD1 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
-          <img src='../images/main_photo.jpg' className='w-[390px] h-[250px] rounded-xl' alt='imgservice1'/>
+          {/* <img src='../images/main_photo.jpg' className='w-[390px] h-[250px] rounded-xl' alt='imgservice1'/> */}
+        <video src='../videos/statics.mp4'   autoPlay
+        loop
+        muted
+        className='w-[390px] h-[250px] rounded-xl'/>
         <h1 className='text-white font-sans text-3xl text-start ml-3'>Service One</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
         </div>
         <div className="CARD2 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
-        <img src='../images/server1.jpg' className='w-[390px] h-[250px] rounded-xl' alt='imgservice1'/>
+        {/* <img src='../images/server1.jpg' className='w-[390px] h-[250px] rounded-xl' alt='imgservice1'/> */}
+        <video src='../videos/barchart.mp4'   autoPlay
+        loop
+        muted
+        className='w-[390px] h-[250px] rounded-xl'/>
         <h1 className='text-white font-sans text-3xl text-start ml-3'>Service Two</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
           </div>
 
           <div className="CARD3 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
-          <img src='../images/server1.jpg' className='w-[390px] h-[250px] rounded-xl' alt='imgservice1'/>
+          {/* <img src='../images/server1.jpg' className='w-[390px] h-[250px] rounded-xl' alt='imgservice1'/> */}
+          <video src='../videos/megaphone.mp4'   autoPlay
+        loop
+        muted
+        className='w-[390px] h-[250px] rounded-xl'/>
           <h1 className='text-white font-sans text-3xl text-start ml-3'>Service Three</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
           </div>
