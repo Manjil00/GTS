7b8ecb8bb701139b5c35096302fb09cc83861ce6
ACER <210220@softwarica.edu.np>
2024-11-05 17:51:32 +0545

    responsive Contactsus

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,16 +6,16 @@ const Contact = () => {
   return (
     <div className='Main-containerContact'>
       <Nav/>
-      <div className="contactPart h-[90vh] md:h-[89vh] p-5 bg-black flex justify-evenly items-center">
+      <div className="contactPart h-[110vh] md:h-[89vh] p-5 bg-Beigebg flex flex-col md:flex-row justify-evenly items-center gap-10">
         <div className="Left">
-          <h1 className="text-3xl text-white font-bold">Contact Us</h1>
-          <p className="text-white text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1 className="text-3xl text-black font-bold">Contact Us</h1>
+          <p className="text-black text-lg">+977-9812345670 / 01-4312345</p>
 
-          <h1 className="text-3xl text-white font-bold">Contact Us</h1>
-          <p className="text-white text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1 className="text-3xl text-black font-bold mt-5">Email Us</h1>
+          <p className="text-black text-lg">gts@gmail.com</p>
         </div>
 
-        <div className="Right h-[500px] w-[500px] bg-slate-600 rounded-xl p-5 flex flex-col justify-center items-center gap-10">
+        <div className="Right h-[500px] w-[400px] md:w-[500px] bg-black rounded-xl p-5 flex flex-col justify-center items-center gap-10">
         <input className='h-[50px] w-[90%] rounded-xl p-4' placeholder='Full Name'></input>
         <input className='h-[50px] w-[90%] rounded-xl p-4' placeholder='Email'></input>
         <input className='h-[50px] w-[90%] rounded-xl p-4' placeholder='Phone No.'></input>
