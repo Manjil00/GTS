e08d3ec01dda32d84f70b809f2a977c619986605
ACER <210220@softwarica.edu.np>
2024-11-05 22:22:56 +0545

    pic addition

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+//COMPONENTS
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+
 const Aboutus = () => {
   return (
-    <div>
+    <div className='Main_aboutus bg-Beigebg w-full h-auto'>
+      <Nav/>
       
+      <div className="body w-full h-auto p-5">
+
+      </div>
+
+      <Footer/>
     </div>
   )
 }
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -13,55 +13,62 @@ const Service = () => {
 
       <div className="card grid grid-cols-1 md:grid-cols-3 justify-center items-center ">
 
-        <div className="CARD1 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
+        <div className="CARD1 bg-black h-[430px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
         <video src='../videos/statics.mp4'   autoPlay
         loop
         muted
         className='w-[390px] h-[250px] rounded-xl'/>
         <h1 className='text-white font-sans text-3xl text-start ml-3'>Service One</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
+        <button className='font-sans bg-white p-2 rounded-xl mt-2 ml-60'>Learn More</button>
         </div>
-        <div className="CARD2 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
+
+        <div className="CARD2 bg-black h-[430px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
         <video src='../videos/https.mp4'   autoPlay
         loop
         muted
         className='w-[390px] h-[250px] rounded-xl'/>
         <h1 className='text-white font-sans text-3xl text-start ml-3'>Service Two</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
+        <button className='font-sans bg-white p-2 rounded-xl mt-2 ml-60'>Learn More</button>
           </div>
 
-          <div className="CARD3 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
+          <div className="CARD3 bg-black h-[430px] w-[400px] rounded-xl mt-8 p-3 cursor-pointer">
           <video src='../videos/megaphone.mp4'   autoPlay
         loop
         muted
         className='w-[390px] h-[250px] rounded-xl'/>
           <h1 className='text-white font-sans text-3xl text-start ml-3'>Service Three</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
+        <button className='font-sans bg-white p-2 rounded-xl mt-2 ml-60'>Learn More</button>
           </div>
 
-          <div className="CARD4 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3  cursor-pointer">
+          <div className="CARD4 bg-black h-[430px] w-[400px] rounded-xl mt-8 p-3  cursor-pointer">
           <video src='../videos/database.mp4'   autoPlay
         loop
         muted
         className='w-[390px] h-[250px] rounded-xl'/>
         <h1 className='text-white font-sans text-3xl text-start ml-3'>Service Four</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
-          </div>
+        <button className='font-sans bg-white p-2 rounded-xl mt-2 ml-60'>Learn More</button>
+        </div>
 
-          <div className="CARD5 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3  cursor-pointer">
+          <div className="CARD5 bg-black h-[430px] w-[400px] rounded-xl mt-8 p-3  cursor-pointer">
           <img src='../images/pic.png' className='w-[390px] h-[250px] rounded-xl' alt='3dpic'/>
           <h1 className='text-white font-sans text-3xl text-start ml-3'>Service Five</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
-          </div>
+        <button className='font-sans bg-white p-2 rounded-xl mt-2 ml-60'>Learn More</button>
+        </div>
 
-          <div className="CARD6 bg-black h-[400px] w-[400px] rounded-xl mt-8 p-3  cursor-pointer">
+          <div className="CARD6 bg-black h-[430px] w-[400px] rounded-xl mt-8 p-3  cursor-pointer">
           <video src='../videos/barchart.mp4'   autoPlay
         loop
         muted
         className='w-[390px] h-[250px] rounded-xl'/>
           <h1 className='text-white font-sans text-3xl text-start ml-3'>Service Six</h1>
         <p className='text-white font-sans text-xs text-start ml-3 mt-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, in aliquid. Porro nesciunt et iusto neque, mollitia doloremque? Eligendi consectetur reprehenderit voluptatibus saepe, ratione dolorum. Totam et quod ipsam veritatis?</p>
-          </div>
+        <button className='font-sans bg-white p-2 rounded-xl mt-2 ml-60'>Learn More</button>
+        </div>
 
 
       </div>
