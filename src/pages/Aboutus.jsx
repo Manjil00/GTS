68a2bd790daaebf7e61a9c237702c8ea30f69f38
ACER <210220@softwarica.edu.np>
2024-11-06 16:56:22 +0545

    all done with responsive aboutus

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -9,12 +9,41 @@ const Aboutus = () => {
     <div className='Main_aboutus bg-Beigebg w-full h-auto'>
       <Nav/>
       
-      <div className="body w-full h-auto p-5 flex justify-evenly items-start gap-10 relative">
-      <img src="../images/ITteam.jpg" className='w-[650px] h-[400px]' alt='teampicture'/>
+      <div className="body w-full h-auto py-5 px-10 flex flex-col md:flex-row justify-evenly items-start gap-10">
+      <img src="../images/ITteam.jpg" className='rounded-xl md:w-[650px] w-[400px] h-[300px] md:h-[400px]' alt='teampicture'/>
       <div className="aboutus">
-      <h1 className='font-titleFont text-4xl absolute left-[550px] top-10'><span className='text-red-700'>ABOUT</span> US</h1>
-      <p className='font-sans mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel debitis porro placeat quasi asperiores quae, dignissimos dicta adipisci, nihil quas repudiandae ratione modi delectus dolorum facilis? Impedit nemo itaque debitis.</p>
+      <h1 className='font-titleFont text-4xl '><span className='text-blue-700'>ABOUT</span> US</h1>
+      <p className='font-sans mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel debitis porro placeat quasi asperiores quae, dignissimos dicta adipisci, nihil quas repudiandae ratione modi delectus dolorum facilis? Impedit nemo itaque debitis.</p>
       </div>
+      </div>
+
+      <div className="mission w-full h-auto flex flex-col justify-evenly items-center px-10 md:px-32 py-5">
+      <h1 className='font-titleFont text-4xl text-center'><span className='text-blue-700'>Our</span> Mission</h1>
+      <p className='font-sans mt-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore maiores sed nesciunt enim, at fugiat reiciendis sapiente aperiam sit alias tenetur deleniti, dignissimos excepturi libero exercitationem laboriosam eius sequi nemo? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore maiores sed nesciunt enim, at fugiat reiciendis sapiente aperiam sit alias tenetur deleniti, dignissimos excepturi libero exercitationem laboriosam eius sequi nemo?</p>
+      </div>
+
+      <div className="mindsmagic">
+
+      <h1 className='font-titleFont text-2xl md:text-4xl text-center mt-5'>Minds Behind the Magic</h1>
+
+      <div className="pictures flex flex-col md:flex-row justify-evenly items-center gap-5 p-5">
+      <div className="card1 rounded-xl w-[250px] h-[300px] bg-slate-400 flex flex-col justify-center items-center">
+        <img src='../images/ITteam.jpg' className='rounded-full w-[90%] h-[80%]' alt='CEO'/>
+        <h1 className="title font-sans text-2xl md:text-4xl mt-3">C.E.O</h1>
+      </div>
+
+      <div className="card2 rounded-xl w-[250px] h-[300px] bg-slate-400 flex flex-col justify-center items-center">
+        <img src='../images/ITteam.jpg' className='rounded-full w-[90%] h-[80%]' alt='CEO'/>
+        <h1 className="title font-sans text-2xl md:text-4xl mt-3">Manager</h1>
+      </div>
+
+      <div className="card3 rounded-xl w-[250px] h-[300px] bg-slate-400 flex flex-col justify-center items-center">
+        <img src='../images/ITteam.jpg' className='rounded-full w-[90%] h-[80%]' alt='CEO'/>
+        <h1 className="title font-sans text-2xl md:text-4xl mt-3">Staff Member</h1>
+      </div>
+      </div>
+      
+
       </div>
 
       <Footer/>
